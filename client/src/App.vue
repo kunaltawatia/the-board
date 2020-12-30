e9b73fb9d8b909f27b0d4b3784e471214b29a365
kunaltawatia <tawatia.1@iitj.ac.in>
2020-12-30 20:06:15 +0530

    sockets initiated

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to Your Vue.js App" />
+	<div id="app">
+		<HelloWorld />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld";
 
 export default {
 	name: "App",
--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+	cors: {
+		origin: "http://localhost:8080",
+		methods: ["GET", "POST"],
+	},
+});
 
 const port = parseInt(process.env.PORT || "3000");
 
@@ -13,6 +18,11 @@ http.listen(port);
 
 io.on("connection", function (socket) {
 	console.log("Connected!");
+
+	socket.on("refreshTime", () => {
+		socket.emit("time", new Date().toUTCString());
+	});
+
 	socket.on("disconnect", () => {
 		console.log("Disconnected!");
 	});
