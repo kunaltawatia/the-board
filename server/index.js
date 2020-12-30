aa8bd81fbf529ed7af31e42d7b42eaea3c273ca4
kunaltawatia <tawatia.1@iitj.ac.in>
2020-12-30 15:47:01 +0530

    Added .prettierrc

--- server/index.js
+++ server/index.js
@@ -6,16 +6,14 @@ const io = require("socket.io")(http);
 const port = parseInt(process.env.PORT || "3000");
 
 app.get("/", function (req, res) {
-  res.send("server running!");
+	res.send("server running!");
 });
 
-http.listen(port, () => {
-  console.log("HTTP server started at " + port );
-});
+http.listen(port);
 
 io.on("connection", function (socket) {
-  console.log("Connected!");
-  socket.on("disconnect", () => {
-    console.log("Disconnected!");
-  });
+	console.log("Connected!");
+	socket.on("disconnect", () => {
+		console.log("Disconnected!");
+	});
 });
